b0e9e527fe56c2d3310fe41c8a888d12144d2dbd
David Otto <david.georg.otto@gmail.com>
2016-06-05 22:37:13 +0200

    sortability implemented

--- src/js/main.js
+++ src/js/main.js
@@ -36,16 +36,44 @@ $.getJSON('players.json', function (data) {
         ));
         var newCard =
             '<li class="card-preview">' +
-                '<div class="name">'+ val.name + " " + val.surname +'</div>' +
-                '<div class="nick">' + val.nick + '</div>' +
-                '<div class="asset">' + val.asset + '</div>' +
+            '<div class="name">' + val.name + " " + val.surname + '</div>' +
+            '<div class="nick">' + val.nick + '</div>' +
+            '<div class="asset">' + val.asset + '</div>' +
             '</li>';
         $(newCard).appendTo("nav ul");
     });
 
     var cards = $("nav ul li.card-preview");
 
-    $(cards).click(function() {
+    $(cards).click(function () {
+        $(cards).removeClass("active");
+        $(this).addClass("active");
+    });
+});
+
+$('#sortAsc').on('click', function () {
+    var sorted = $("nav ul li.card-preview").sort(function (a, b) {
+        return $(a).find(".name").text() > $(b).find(".name").text();
+    });
+    $("nav ul").html(sorted);
+
+    var cards = $("nav ul li.card-preview");
+
+    $(cards).click(function () {
+        $(cards).removeClass("active");
+        $(this).addClass("active");
+    });
+});
+
+$('#sortDesc').on('click', function () {
+    var sorted = $("nav ul li.card-preview").sort(function (a, b) {
+        return $(a).find(".name").text() < $(b).find(".name").text();
+    });
+    $("nav ul").html(sorted);
+
+    var cards = $("nav ul li.card-preview");
+
+    $(cards).click(function () {
         $(cards).removeClass("active");
         $(this).addClass("active");
     });
